71c4d05dd8ae42fc54cf2c613af0bdbe1aaa20c0
Yuwen Hu <yuwen.hu@intel.com>
2023-09-05 17:19:43 +0800

    Add default structure for bigdl assets document

---
+++ readthedocs/source/conf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# This file does only contain a selection of the most common options. For a
+# full list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+
+project = 'BigDL Assets'
+copyright = '2020, BigDL Authors'
+author = 'BigDL Authors'
